c03c63f87a2e69b1edbdb58cbce8788ad2530f31
songseogyeong <0307ssk@naver.com>
2025-07-08 23:54:28 +0900

    day2

---
+++ 1_입문/day1/java-start/src/variable/Var1.java
@@ -0,0 +1,11 @@
+// java 파일이 위치하는 package와 소속 package를 꼭 맞춰줘야 한다.
+package variable;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}

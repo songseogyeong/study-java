a36390015d52ff7e1e7e6d3f64085669a001b011
songseogyeong <0307ssk@naver.com>
2025-07-10 00:19:33 +0900

    day3 - variable

---
+++ 1_입문/day1/java-start/src/variable/Var4.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var4 {
+
+    public static void main(String[] args) {
+
+        // 변수는 하나씩 선언 가능
+        int a;
+        int b;
+
+        // 한번에 선언도 가능
+        int c, d;
+
+
+    }
+
+
+}
---
+++ 1_입문/day1/java-start/src/variable/Var6.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+
+        // System.out.println(a);
+
+        // 오류 코드 : variable a might not have been initialized
+        // 지역 변수를 초기화를 하지 않고 println 하려고 해서 컴파일 오류
+        // 자바는 지역 변수를 초기화하도록 강제함
+    }
+}
